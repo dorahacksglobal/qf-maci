5e38735dab7b5f0b2a0a880ef164da85faee65db
tari404 <2680383605@qq.com>
2022-01-28 19:27:04 +0800

    🗒 add logs

--- js/poseidon-code.js
+++ js/poseidon-code.js
@@ -10,20 +10,3 @@ const nInputs = Number(process.argv[2])
 console.log(nInputs)
 
 console.log(poseidonGenContract.createCode(nInputs))
-
-// local ganache deployed
-// PoseidonT3: 0x0049aace27f85e898b1e3cbc31c5b8c00a1c1182
-// PoseidonT6: 0x6f616794e9c4fdd27b6a9a74fd16348ac4884095
-
-// VkRegistry: 0x94bc0a14b92378C694923f6C2C584A1FC560e5DC
-// Verifier:   0x0FEC94A47b6A56B422945680CB2025a0bE28CdA5
-// QTreeRoot:  0x228C2b240d33241fC9c20Fa8cdb0aF3C7C2Df68f
-
-// BSC dev deployed
-// PoseidonT3: 0xe09994f9c67f67bb78c048ab9797235fb523310f
-// PoseidonT6: 0xe3362fa37635dfe95dc6f408e4165014cefe7c2c
-
-// MACI Lib:   0xE87484f79A361662859809192FBd05b922093aAC
-// VkRegistry: 0x4EDc150E3D1CcE870391CBd2b0C50d2508886325
-// Verifier:   0x5997da84050d56F491e93F546CD1A11Cb17c5246
-// QTreeRoot:  0xEF7c6e42A31f76dE2F2228A5705b7e5c5AE6D2B6
